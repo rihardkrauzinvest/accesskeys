d3adf1714c58428b576010c880ad026f181c3436
Evgeny Chkalov <echkalov@intermedia.net>
2020-07-05 14:10:35 +0300

    init

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use('/', express.static(path.join(__dirname, 'dist')));
 
 app.listen(port, () => {
