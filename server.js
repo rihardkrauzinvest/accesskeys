d35f0143c269d6d8578e95c489184e44ef597d9d
Evgeny Chkalov <echkalov@intermedia.net>
2020-07-05 14:04:09 +0300

    init

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static(path.join(__dirname, 'dist/rk-ui')));
+app.use('/', express.static(path.join(__dirname, 'dist')));
 
 app.listen(port, () => {
   console.log(`Server has been started on port ${port}`);
